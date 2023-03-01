a2fa2d9b37cfbcb14db70b0a94d678d1791b6cba
Meghana Puligalla <meghanapuligalla@Meghanas-MacBook-Air.local>
2023-03-01 08:54:37 -0600

    renamed api folder name

---
+++ SocialMediaRestApi/Controllers/SampleTweetsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestRestApp.Controllers
+{
+    [Route("api/sample-tweets")]
+    [ApiController]
+    public class SampleTweetsController : ControllerBase
+    {
+        [Route("collect")]
+        [HttpPost]
+        public async Task<int> CollectSampleTweets()
+        {
+            return await Task.FromResult<int>(GetSum(2, 3));
+        }
+
+        [Route("show")]
+        [HttpPost]
+        public async Task<int> ShowSampleTweets()
+        {
+            return await Task.FromResult<int>(GetSum(2, 3));
+        }
+
+
+        private int GetSum(int x, int y)
+        {
+            return x+y;
+        }
+    }
+}
---
+++ SocialMediaRestApi/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SocialMediaContract;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaManager.Dependency;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace SocialMediaRestApi
+{
+	public class StartUp
+	{
+		public IConfiguration Configuration { get; }
+		public StartUp(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+			services.AddOptions();
+			services.AddSingleton<ILogger>(sp =>
+			{
+				var logger = sp.GetService<ILogger<ControllerBase>>();
+				return logger;
+			});
+			services.AddApplicationInsightsTelemetry(Configuration);
+			ManagerDependency.Register(services);
+			//services.AddAuthentication().AddOAuth("", null); -- get token parameters from config for token validaiton.
+        }
+
+	}
+}
+
